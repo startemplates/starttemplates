34a5fb209db3bc800791cf5d001e27ec382bb1b6
startemplates <61298881+startemplates@users.noreply.github.com>
2020-02-21 02:09:37 +0200

    Create star.js

---
+++ star.js
@@ -0,0 +1,17 @@
+<script>
+$(document)['ready'](function() {
+    setInterval(function() {
+        if (!$('#mycopyrights:visible')['length']) {
+            window['location']['href'] = 'http://www.star-templates.com/'
+        }
+    }, 3000)
+});
+window['onload'] = function() {
+    var _0x9f43x1 = document['getElementById']('mycopyrights');
+    _0x9f43x1['setAttribute']('href', 'http://www.star-templates.com/');
+    _0x9f43x1['setAttribute']('rel', 'dofollow');
+    _0x9f43x1['setAttribute']('title', 'Free Blogger Templates');
+    _0x9f43x1['setAttribute']('style', 'display: inline-block!important; font-size: inherit!important; color: #ffbd2f!important; visibility: visible!important;z-index:99!important; opacity: 1!important;');
+    _0x9f43x1['innerHTML'] = 'StarTemplates'
+};
+</script>
